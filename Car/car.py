d38d3d22415d91fadb2e70b61e7c344987313105
lightswitch2020 <ribosome.pdb2020@gmail.com>
2022-09-26 16:12:28 +0200

    creating class functions

--- Car/car.py
+++ Car/car.py
@@ -4,4 +4,26 @@ class Car:
         self.__model = ""
         self.__lights = False
         self.__direction = 0
-    def
+        self.__fuel = 0
+        self.__on = False
+    def increaseVelocity(self):
+        self.__currentVelocity += 5
+    def decreaseVelociy(self):
+        self.__currentVelocity -= 5
+    def setCurrentVelocity(self , velocity):
+        self.__currentVelocity = velocity
+    def getCurrentVelocity(self):
+        return self.__currentVelocity
+    def setModel(self , model):
+        self.__model = model
+    def setLights(self , b):
+        self.__lights = b
+    def brake(self):
+        self.__currentVelocity = 0
+    def turnOn(self):
+        self.__on = True
+    def getModel(self):
+        return self.__model
+    def setModel(self , model):
+        self.__model = model
+
